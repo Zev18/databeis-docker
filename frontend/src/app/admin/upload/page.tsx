fd8b20ba163f53e2806cd7e31e6cc56176c17e28
zev18 <zevross@gmail.com>
2024-05-13 22:14:42 -0400

    added auth check to bulk upload page

--- frontend/src/app/admin/upload/page.tsx
+++ frontend/src/app/admin/upload/page.tsx
@@ -1,6 +1,19 @@
+import { headers } from "next/headers";
 import FileForm from "./FileForm";
+import { apiUrlServer } from "@/lib/consts";
+import { redirect } from "next/navigation";
 
-export default function Upload() {
+export default async function Upload() {
+  const res = await fetch(apiUrlServer + "/api/authenticate", {
+    credentials: "include",
+    headers: headers(),
+  });
+  const user = await res.json();
+
+  if (!user.isAdmin) {
+    redirect("/login");
+  }
+
   return (
     <div className="flex w-full flex-col items-center gap-4 p-4">
       <FileForm />
