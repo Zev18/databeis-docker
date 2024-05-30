9deee1ab551f0c62ce62095bbbcc3a68e99f6978
zev18 <zevross@gmail.com>
2024-05-30 15:26:31 -0400

    link

--- frontend/src/lib/consts.ts
+++ frontend/src/lib/consts.ts
@@ -2,7 +2,7 @@ export const apiUrlServer =
   process.env.NEXT_PUBLIC_API_URL || "http://goapp:8000";
 export const apiUrlClient =
   process.env.NODE_ENV == "production"
-    ? "http://goapp:8000"
+    ? "http://api.databeis.zevross.dev"
     : "http://localhost:8000";
 
 export const delimiter = ";";
